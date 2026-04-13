9d76f770b6d2066edddaf9d2f4fa8eccc5aa8c07
harshithppillai <harshithppillai07@gmail.com>
2026-04-13 23:34:04 +0530

    Added removing trailing whitespace plugin

--- init.lua
+++ init.lua
@@ -261,6 +261,7 @@ require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'NMAC427/guess-indent.nvim', -- Detect tabstop and shiftwidth automatically
   -- in your plugins list
+  'cappyzawa/trim.nvim',
 
   {
     'glebzlat/arduino-nvim',
